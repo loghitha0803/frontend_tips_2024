3fc20573ccd854d2c918d0e08111bffb8ec71300
Loghitha sankar <loghitha.sankar@solitontech.local>
2024-01-24 11:30:44 +0530

    refactor:fix the error (backend)

--- src/js/node.js
+++ src/js/node.js
@@ -32,105 +32,97 @@ document.querySelector('.image-city').addEventListener('change', (city) => {
     const farenheit = Math.round(Number(tempvalue) * 1.8 + 32, 0)
     farenheitValue.innerText = `${farenheit}F`
 
-    /**
-     *
-     * @param cityDateTime
-     */
-    function formatDateTime (cityDateTime) {
-      const [date, time] = cityDateTime.split(', ')
-      const [month, datesplit, year] = date.split('/')
-      const [hrs, mins, secs] = time.split(':')
-      let [sec, am] = secs.split(' ')
-      document.querySelector('.hrs-mins').innerText = `${hrs}:${mins}`
-      document.querySelector('.seconds').innerText = `:${sec}`
-      document.querySelector(
-        '.date'
-      ).innerText = `${datesplit}/${month}/${year}`
-      const amImage = document.querySelector('.am-image')
-      if (am === 'AM') {
-        amImage.src = '../../../Assets/General Images & Icons/amState.svg'
-      } else {
-        amImage.src = '../../../Assets/General Images & Icons/pmState.svg'
-      }
-      const firstTime = document.querySelectorAll('.first-time')
-      const nextFiveHours = Array.from(firstTime).map(
-        (element) => element.textContent
-      )
-      for (let i = 0, currentTime = hrs; i < nextFiveHours.length; i++) {
-        if (am == 'AM') {
-          if (currentTime < 11) {
-            currentTime++
-            nextFiveHours[i] = currentTime + 'AM'
-            firstTime[i].textContent = nextFiveHours[i]
-          } else if (currentTime == 11) {
-            currentTime++
-            nextFiveHours[i] = currentTime + 'PM'
-            am = 'PM'
-            firstTime[i].textContent = nextFiveHours[i]
-          } else if (currentTime == 12) {
-            nextFiveHours[i] = 1 + 'AM'
-            currentTime = 1
-            am = 'AM'
-            firstTime[i].textContent = nextFiveHours[i]
-          }
-        } else {
-          if (currentTime < 11) {
-            currentTime++
-            nextFiveHours[i] = currentTime + 'PM'
-            firstTime[i].textContent = nextFiveHours[i]
-          } else if (currentTime == 11) {
-            currentTime++
-            nextFiveHours[i] = currentTime + 'AM'
-            am = 'AM'
-            firstTime[i].textContent = nextFiveHours[i]
-          } else if (currentTime == 12) {
-            nextFiveHours[i] = 1 + 'PM'
-            currentTime = 1
-            am = 'PM'
-            firstTime[i].textContent = nextFiveHours[i]
-          }
-        }
-      }
-      const hourly = document.querySelectorAll('.hourly-image')
-      const hourlyImage = Array.from(hourly).map((element) => element.src)
-      const updatedTimings = cityData[givenInput].nextFiveHrs
-      updatedTimings.unshift(cityData[givenInput].temperature)
-
-      for (let i = 0; i < updatedTimings.length; i++) {
-        updatedTimings[i] = updatedTimings[i].slice(0, -2)
-        if (
-          Number(updatedTimings[i]) >= 23 &&
-          Number(updatedTimings[i]) <= 29
-        ) {
-          hourlyImage[i] = '../../../Assets/Weather Icons/cloudyIcon.svg'
-        } else if (
-          Number(updatedTimings[i]) >= 18 &&
-          Number(updatedTimings[i]) <= 22
-        ) {
-          hourlyImage[i] = '../../../Assets/Weather Icons/windyIcon.svg'
-        } else if (Number(updatedTimings[i]) < 18) {
-          hourlyImage[i] = '../../../Assets/Weather Icons/rainyIcon.svg'
-        } else if (Number(updatedTimings[i])) {
-          hourlyImage[i] = '../../../Assets/Weather Icons/sunnyIcon.svg'
-        }
-      }
-      const tempHourly = document.querySelectorAll(
-        '.temperature-hourly-update'
-      )
-      const tempHourlyUpdate = Array.from(tempHourly).map(
-        (element) => element.textContent
-      )
-      for (let i = 0; i < tempHourlyUpdate.length; i++) {
-        tempHourly[i].textContent = updatedTimings[i] + '\u00B0C'
-      }
-      updatedTimings.shift()
-      document.querySelector('.now-image').src = hourlyImage[0]
-      document.querySelector('.first-hour-image').src = hourlyImage[1]
-      document.querySelector('.second-image').src = hourlyImage[2]
-      document.querySelector('.third-image').src = hourlyImage[3]
-      document.querySelector('.forth-image').src = hourlyImage[4]
-      setTimeout(formatDateTime, 1000)
+    const [date, time] = cityData[givenInput].dateAndTime.split(', ')
+    const [month, datesplit, year] = date.split('/')
+    const [hrs, mins, secs] = time.split(':')
+    let [sec, am] = secs.split(' ')
+    document.querySelector('.hrs-mins').innerText = `${hrs}:${mins}`
+    document.querySelector('.seconds').innerText = `:${sec}`
+    document.querySelector(
+      '.date'
+    ).innerText = `${datesplit}/${month}/${year}`
+    const amImage = document.querySelector('.am-image')
+    if (am === 'AM') {
+      amImage.src = '../../../Assets/General Images & Icons/amState.svg'
+    } else {
+      amImage.src = '../../../Assets/General Images & Icons/pmState.svg'
     }
-    formatDateTime(cityData[givenInput].dateAndTime)
+    const firstTime = document.querySelectorAll('.first-time')
+    const nextFiveHours = Array.from(firstTime).map(
+      (element) => element.textContent
+    )
+    for (let i = 0, currentTime = hrs; i < nextFiveHours.length; i++) {
+      if (am == 'AM') {
+        if (currentTime < 11) {
+          currentTime++
+          nextFiveHours[i] = currentTime + 'AM'
+          firstTime[i].textContent = nextFiveHours[i]
+        } else if (currentTime == 11) {
+          currentTime++
+          nextFiveHours[i] = currentTime + 'PM'
+          am = 'PM'
+          firstTime[i].textContent = nextFiveHours[i]
+        } else if (currentTime == 12) {
+          nextFiveHours[i] = 1 + 'AM'
+          currentTime = 1
+          am = 'AM'
+          firstTime[i].textContent = nextFiveHours[i]
+        }
+      } else {
+        if (currentTime < 11) {
+          currentTime++
+          nextFiveHours[i] = currentTime + 'PM'
+          firstTime[i].textContent = nextFiveHours[i]
+        } else if (currentTime == 11) {
+          currentTime++
+          nextFiveHours[i] = currentTime + 'AM'
+          am = 'AM'
+          firstTime[i].textContent = nextFiveHours[i]
+        } else if (currentTime == 12) {
+          nextFiveHours[i] = 1 + 'PM'
+          currentTime = 1
+          am = 'PM'
+          firstTime[i].textContent = nextFiveHours[i]
+        }
+      }
+    }
+    const hourly = document.querySelectorAll('.hourly-image')
+    const hourlyImage = Array.from(hourly).map((element) => element.src)
+    const updatedTimings = cityData[givenInput].nextFiveHrs
+    updatedTimings.unshift(cityData[givenInput].temperature)
+
+    for (let i = 0; i < updatedTimings.length; i++) {
+      updatedTimings[i] = updatedTimings[i].slice(0, -2)
+      if (
+        Number(updatedTimings[i]) >= 23 &&
+          Number(updatedTimings[i]) <= 29
+      ) {
+        hourlyImage[i] = '../../../Assets/Weather Icons/cloudyIcon.svg'
+      } else if (
+        Number(updatedTimings[i]) >= 18 &&
+          Number(updatedTimings[i]) <= 22
+      ) {
+        hourlyImage[i] = '../../../Assets/Weather Icons/windyIcon.svg'
+      } else if (Number(updatedTimings[i]) < 18) {
+        hourlyImage[i] = '../../../Assets/Weather Icons/rainyIcon.svg'
+      } else if (Number(updatedTimings[i])) {
+        hourlyImage[i] = '../../../Assets/Weather Icons/sunnyIcon.svg'
+      }
+    }
+    const tempHourly = document.querySelectorAll(
+      '.temperature-hourly-update'
+    )
+    const tempHourlyUpdate = Array.from(tempHourly).map(
+      (element) => element.textContent
+    )
+    for (let i = 0; i < tempHourlyUpdate.length; i++) {
+      tempHourly[i].textContent = updatedTimings[i] + '\u00B0C'
+    }
+    updatedTimings.shift()
+    document.querySelector('.now-image').src = hourlyImage[0]
+    document.querySelector('.first-hour-image').src = hourlyImage[1]
+    document.querySelector('.second-image').src = hourlyImage[2]
+    document.querySelector('.third-image').src = hourlyImage[3]
+    document.querySelector('.forth-image').src = hourlyImage[4]
   } else { alert('wrong city name entered') }
 })
